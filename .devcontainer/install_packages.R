12c15b3b4161e2a67d39dd89d8abf1ec7ac39b35
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2025-05-26 19:33:27 +0200

    wip-2

---
+++ .devcontainer/install_packages.R
@@ -0,0 +1,21 @@
+# Install CRAN packages
+install.packages(
+  c(
+    "Rcpp", "BH", "httr", "googledrive", "googlesheets4", "ragg",
+    "rvest", "tidyverse", "readr", "pheatmap", "kableExtra", "plotly",
+    "rmarkdown", "factoextra", "ggpubr", "remotes"
+  ),
+  dependencies = TRUE,
+  repos = "https://cloud.r-project.org"
+)
+
+# Install Bioconductor packages
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager", repos = "https://cloud.r-project.org")
+
+BiocManager::install(
+  c(
+    "DESeq2", "AnnotationDbi", "org.Mm.eg.db", "msigdbr",
+    "clusterProfiler", "DOSE", "europepmc", "enrichplot", "fgsea"
+  )
+)
