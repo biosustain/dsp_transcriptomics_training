41db75c672897caee614fcfbfac7261c6afaf799
Juliana Assis <julianaassis17@gmail.com>
2026-05-11 10:22:35 +0000

    devcontainer

---
+++ .devcontainer/install_packages.R
@@ -0,0 +1,26 @@
+install.packages(
+  c(
+    "languageserver",  # R language server for VS Code
+    "ggpubr",          # plot theming
+    "fgsea",           # GSEA
+    "mulea"            # ORA
+  ),
+  dependencies = TRUE,
+  repos = "https://cloud.r-project.org",
+  Ncpus = parallel::detectCores()
+)
+
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
+  install.packages("BiocManager", repos = "https://cloud.r-project.org")
+}
+
+BiocManager::install(
+  c(
+    "DESeq2",
+    "apeglm",
+    "EnhancedVolcano",
+    "KEGGREST"
+  ),
+  update = FALSE,
+  ask = FALSE
+)
