c0f2b8092076e680360f39f9800af95d4eb1dbe0
Sebastian Schulz <sebschu@biosustain.dtu.dk>
2026-05-15 17:48:13 +0200

    replacing relative by absolute paths in github workspace; testing succeeded with 2 samples (3 mins) and all 6 samples (14 mins); 2 sample data processing is kept due to short processing time

--- 01_scripts/00_nfcore_rnaseq_processing.sh
+++ 01_scripts/00_nfcore_rnaseq_processing.sh
@@ -1,11 +1,12 @@
-# script to process RNA sequencing files
+# Script to process RNA sequencing files
 # We are using v3.23.0 of the nf-core/rnaseq pipeline with the profile "prokaryotic"
+# A custom config file is used here to limit the number of CPUs and memory
 nextflow run 'https://github.com/nf-core/rnaseq' \
-    -name 'Ecoli_MG1655_saccharin' \
-    --outdir './results/nfcore_rnaseq_processing_subsampled' \
-    --input './data/seq_files_subsampled/samplesheet_50k_subsampled_2samples.csv' \
-    --fasta './data/genome_files/GCF_000005845.2_ASM584v2_genomic.fna.gz' \
-    --gtf './data/genome_files/GCF_000005845.2_ASM584v2_genomic.gtf.gz' \
+    -name 'Ecoli_MG1655_saccharin_2_samples' \
+    --outdir '/workspaces/dsp_transcriptomics_training/results/nfcore_rnaseq_processing_subsampled' \
+    --input '/workspaces/dsp_transcriptomics_training/data/seq_files_subsampled/samplesheet_50k_subsampled_2samples.csv' \
+    --fasta '/workspaces/dsp_transcriptomics_training/data/genome_files/GCF_000005845.2_ASM584v2_genomic.fna.gz' \
+    --gtf '/workspaces/dsp_transcriptomics_training/data/genome_files/GCF_000005845.2_ASM584v2_genomic.gtf.gz' \
     -r 3.23.0 \
     -profile prokaryotic,docker \
-    -c ./custom.config 
+    -c /workspaces/dsp_transcriptomics_training/01_scripts/custom.config 
